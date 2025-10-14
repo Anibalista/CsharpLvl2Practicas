6da74e6bcbcc35f867c40d8265668d8a82a3b56a
Anibalista <110265813+Anibalista@users.noreply.github.com>
2025-10-14 12:13:37 -0300

    modificaciones02

---
+++ PracticasCursoCSharp/Practica02/Disco.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica02
+{
+    public class Disco
+    {
+        //Atributos
+        private int id;
+        private string titulo;
+
+        public Disco(int id)
+        {
+            this.id = id;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public override string ToString()
+        {
+            return titulo;
+        }
+
+        public string Titulo{
+            get { return titulo; }
+            set { titulo = value; }
+        }
+
+        public DateTime FechaLanzamiento { get; set; }
+        public int CantidadCanciones { get; set; }
+        public string UrlImagenTapa { get; set; }
+        public string Estilo { get; set; }
+        public string TipoEdicion { get; set; }
+    }
+}
